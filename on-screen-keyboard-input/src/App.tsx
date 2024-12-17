237d960ae5c2b6293c2dedfbe4f280697b415796
David Zahiri <dzahiri@posnation.com>
2024-12-17 11:56:49 -0500

    save

--- on-screen-keyboard-input/src/App.tsx
+++ on-screen-keyboard-input/src/App.tsx
@@ -1,14 +1,56 @@
+import { ChangeEvent, FocusEventHandler, MouseEventHandler, useCallback, useState } from 'react'
 import './App.css'
 import { Input } from './Input'
 import { KeypadInput } from './KeypadInput'
 
 function App() {
+  const [values, setValues] = useState<string>('')
+
+
+  const onChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+
+    if (event.target.name === 'numberinput') {
+      // event.preventDefault();
+      if (event.target.value === 'C') {
+        setValues('')
+      } else {
+        setValues((prev) => prev + event.target.value)
+      }
+    } else {
+      setValues(event.target.value)
+    }
+
+    console.log('event.target.value:', event.target.value);
+    console.log('event.target.name:', event.target.name);
+    console.log('event.target.selectionStart:', event.target.selectionStart);
+
+
+  }, []);
+
+  const handleClick = (key, input) => {
+
+  }
+
+  const onClick: MouseEventHandler<HTMLInputElement> = (e) => {
+    console.log('click event', e)
+  }
+  const onMouseDown: MouseEventHandler<HTMLInputElement> = (e) => {
+    e.preventDefault();
+    console.log('click event', e)
+  }
+  const onButtonFocus: FocusEventHandler<HTMLInputElement> = (e) => {
+
+    console.log('focus event', e)
+  }
+  const onFocus: FocusEventHandler<HTMLInputElement> = (e) => {
+    console.log('focus event', e)
+  }
 
   return (
     <>
       <div>
-        <Input />
-        <KeypadInput />
+        <Input onChange={onChange} name="input" value={values} onClick={onClick} onFocus={onFocus} />
+        <KeypadInput onChange={onChange} onMouseDown={onMouseDown} name="numberinput" onFocus={onButtonFocus} />
       </div>
     </>
   )
--- on-screen-keyboard-input/src/KeypadInput.tsx
+++ on-screen-keyboard-input/src/KeypadInput.tsx
@@ -1,37 +1,34 @@
-import { FC, InputHTMLAttributes } from 'react';
+import { ChangeEventHandler, FC, InputHTMLAttributes, MouseEventHandler } from 'react';
 import { Flex } from './Flex';
 import { KeypadInputButton } from './KeypadInput.styled';
 
-export interface IKeypadInputProps extends InputHTMLAttributes<HTMLInputElement> { }
+export interface IKeypadInputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'name'> { name: string; onChange: ChangeEventHandler<HTMLInputElement> }
 
-export const KeypadInput: FC<IKeypadInputProps> = (props) => {
+export const KeypadInput: FC<IKeypadInputProps> = ({ onChange, name, ...restProps }) => {
+	const _onChange: MouseEventHandler<HTMLInputElement> = (event) => {
+		onChange({ ...event, target: { ...event.target, name } as EventTarget & HTMLInputElement })
+	}
 	return (
 		<Flex flexDirection="column" gap="4px" width="100%">
-			{/* Container for the keypad, organized in a column with gaps */}
-			{/* Each row of the keypad contains three buttons, each associated with a numeric value or the clear ('C') button */}
 			<Flex gap="4px" width="100%">
-				{/* First row of the keypad: buttons for 7, 8, and 9 */}
-				<KeypadInputButton value="7" /> {/* Button for 7 */}
-				<KeypadInputButton value="8" /> {/* Button for 8 */}
-				<KeypadInputButton value="9" /> {/* Button for 9 */}
+				<KeypadInputButton {...restProps} onClick={_onChange} name={name} value="7" />
+				<KeypadInputButton {...restProps} onClick={_onChange} name={name} value="8" />
+				<KeypadInputButton {...restProps} onClick={_onChange} name={name} value="9" />
 			</Flex>
 			<Flex gap="4px" width="100%">
-				{/* Second row of the keypad: buttons for 4, 5, and 6 */}
-				<KeypadInputButton value="4" /> {/* Button for 4 */}
-				<KeypadInputButton value="5" /> {/* Button for 5 */}
-				<KeypadInputButton value="6" /> {/* Button for 6 */}
+				<KeypadInputButton {...restProps} onClick={_onChange} name={name} value="4" />
+				<KeypadInputButton {...restProps} onClick={_onChange} name={name} value="5" />
+				<KeypadInputButton {...restProps} onClick={_onChange} name={name} value="6" />
 			</Flex>
 			<Flex gap="4px" width="100%">
-				{/* Third row of the keypad: buttons for 1, 2, and 3 */}
-				<KeypadInputButton value="1" /> {/* Button for 1 */}
-				<KeypadInputButton value="2" /> {/* Button for 2 */}
-				<KeypadInputButton value="3" /> {/* Button for 3 */}
+				<KeypadInputButton {...restProps} onClick={_onChange} name={name} value="1" />
+				<KeypadInputButton {...restProps} onClick={_onChange} name={name} value="2" />
+				<KeypadInputButton {...restProps} onClick={_onChange} name={name} value="3" />
 			</Flex>
 			<Flex gap="4px" width="100%">
-				{/* Fourth row of the keypad: buttons for 0, 00, and C (clear) */}
-				<KeypadInputButton value="0" /> {/* Button for 0 */}
-				<KeypadInputButton value="00" /> {/* Button for 00 */}
-				<KeypadInputButton value="C" /> {/* Button for clearing the input */}
+				<KeypadInputButton {...restProps} onClick={_onChange} name={name} value="0" />
+				<KeypadInputButton {...restProps} onClick={_onChange} name={name} value="00" />
+				<KeypadInputButton {...restProps} onClick={_onChange} name={name} value="C" />
 			</Flex>
 		</Flex>
 	);
